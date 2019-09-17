cc6b07bf8fb2b7b2ae6493faa09b4ac9f8b6d6da
wx_kl <wx_9de33a9a01e6450284e000251708bf70@git.code.tencent.com>
2019-09-17 15:22:30 +0800

    s

--- server/controller/home.js
+++ server/controller/home.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 router.get('/home', (req, res) => {
     let sql = `SELECT * FROM slider`
     mydb.query(sql, (err, result) => {
